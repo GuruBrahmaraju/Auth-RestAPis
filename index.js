ea0a4f9aa19d3c9335da32c8d706d727dcd5a89d
Gurubrahmaraju <gurubrahmac@gmail.com>
2020-01-23 14:47:18 +0530

    Auth RoutingsetUp

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
+"use strict";
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 //import Routes
 const authRoute = require('./routes/auth');
+// Connect to mongoose
+
+mongoose.Promise = global.Promise;
+
+var db = mongoose.connection;
+
+// Added check for DB connection
+
+if(!db)
+    console.log("Error connecting db")
+else
+    console.log("Db connected successfully")
+
+dotenv.config();
+
+
+mongoose.connect('mongodb+srv://devGuru:Guru@1234@cluster0-rcsfx.mongodb.net/test?retryWrites=true&w=majority',
+{ useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true })
+.then(() => console.log('DB Connected!'))
+.catch(err => console.error(err));
+
+
+
+
 
 app.use('/api/user', authRoute);
 
